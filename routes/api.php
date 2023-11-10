620a3a3c6be267396474aa8b4bbe38cca8570d4d
Martynas L <martynas.l@mipsas.com>
2023-11-10 15:47:54 +0200

    Add basic api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\BookingController;
+use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\ProfessionController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\UserHistoryController;
+use App\Http\Controllers\Api\PaymentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +19,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->User();
+// });
+
+// Bookings Routes
+Route::apiResource('bookings', BookingController::class);
+
+// // Company Routes
+// Route::apiResource('companies', CompanyController::class);
+
+// // Profession Routes
+// Route::apiResource('professions', ProfessionController::class);
+
+// // User Routes
+// Route::apiResource('users', UserController::class);
+
+// // User History Routes
+// Route::apiResource('user_histories', UserHistoryController::class);
+
+// // Payment Routes
+// Route::apiResource('payments', PaymentController::class);
+
